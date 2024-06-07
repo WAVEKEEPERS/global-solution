b91176c1818384ce22890d2761b3e04c576f26a9
P3terHenry <pedro.antonieti@outlook.com>
2024-06-07 11:53:04 -0300

    Realizado criação da função cadastrar para validar se os campos estão preenchidos, realizar unshift na lista armazenada no LocalStorage e adiciona novamente com os novos logins cadastrados.

--- source/js/register.js
+++ source/js/register.js
@@ -26,3 +26,43 @@ let usuario = JSON.parse(localStorage.getItem("usuario-logado"));
 if (usuario) {
 window.location.href = "../../pages/quiz.html";
 }
+
+function cadastrar(nome, cpf, email, senha) {
+
+    let msgStatus = document.querySelector(".valida");
+  
+    let logins = JSON.parse(localStorage.getItem("logins"));
+  
+    for (let x = 0; x < logins.length; x++) {
+  
+        if ((email.value == logins[x].emailUsuario)) {
+            msgStatus.setAttribute("class", "alert icon-alert");
+            msgStatus.innerText = "E-mail já cadastrado!";
+            setTimeout(function() {
+                msgStatus.setAttribute("class", "valida");
+                msgStatus.innerText = "";
+            }, 3000);
+            return false;
+  
+        }
+    }
+  
+    logins.unshift({
+        nomeCompleto: nome.value,
+        emailUsuario: email.value,
+        cpfUsuario: cpf.value,
+        senhaUsuario: senha.value
+    }, );
+  
+    msgStatus.setAttribute("class", "alert icon-alert");
+    msgStatus.innerText = "Cadastro realizado com sucesso!";
+  
+    localStorage.setItem("logins", JSON.stringify(logins));
+  
+    setTimeout(function() {
+        msgStatus.setAttribute("class", "valida");
+        msgStatus.innerText = "";
+        window.location.href = "../index.html";
+    }, 3000);
+    return false;
+}
