3e305b88a56bf619dfe0cca07e81d6b9cf3d7c39
P3terHenry <pedro.antonieti@outlook.com>
2024-06-07 11:46:38 -0300

    Adicionado lista com usuáriros pré-definidos.

---
+++ source/js/register.js
@@ -0,0 +1,17 @@
+let listaUsuario = [{
+    nomeCompleto: "Pedro Antonieti",
+    emailUsuario: "rm556253@fiap.com.br",
+    cpfUsuario: "12345678910",
+    senhaUsuario: "556253",
+  }, {
+    nomeCompleto: "Letícia Passos",
+    emailUsuario: "rm555241@fiap.com.br",
+    cpfUsuario: "12345678910",
+    senhaUsuario: "555241",
+  }, {
+    nomeCompleto: "André Altobelli",
+    emailUsuario: "rm554764@fiap.com.br",
+    cpfUsuario: "12345678910",
+    senhaUsuario: "554764",
+  }, ];
+
