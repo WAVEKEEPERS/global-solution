22af1a48ec7cbc202ac9a8702d5309ffe5c3b2d6
P3terHenry <pedro.antonieti@outlook.com>
2024-06-07 11:43:55 -0300

    Realizado criação da função validar para verificação do dados de e-mail e senha corretos.

--- source/js/login.js
+++ source/js/login.js
@@ -26,3 +26,33 @@ let usuario = JSON.parse(localStorage.getItem("usuario-logado"));
 if (usuario) {
     window.location.href = "../../pages/quiz.html";
 }
+
+function validar(inputEmail, inputSenha) {
+
+    let msgStatus = document.querySelector(".valida");
+  
+    for (let x = 0; x < logins.length; x++) {
+  
+        if ((inputEmail.value == logins[x].emailUsuario) && (inputSenha.value == logins[x].senhaUsuario)) {
+  
+            msgStatus.setAttribute("class", "alert icon-alert");
+            msgStatus.innerText = "Login realizado com sucesso!";
+  
+            localStorage.setItem("usuario-logado", JSON.stringify(logins[x]));
+  
+            setTimeout(function() {
+                msgStatus.setAttribute("class", "valida");
+                msgStatus.innerText = "";
+                window.location.href = "../../pages/quiz.html";
+            }, 3000);
+            return false;
+        }
+    }
+    msgStatus.setAttribute("class", "alert icon-alert");
+    msgStatus.innerText = "Login ou senha invalidos!";
+    setTimeout(function() {
+        msgStatus.setAttribute("class", "valida");
+        msgStatus.innerText = "";
+    }, 3000);
+    return false;
+  }
