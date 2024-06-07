7b47b596472b05a1407bb5b49606916f8681a625
P3terHenry <pedro.antonieti@outlook.com>
2024-06-07 11:42:44 -0300

    Adicionado lista com usuáriros pré-definidos.

---
+++ source/js/login.js
@@ -0,0 +1,28 @@
+let listaUsuario = [{
+    nomeCompleto: "Pedro Antonieti",
+    emailUsuario: "rm556253@fiap.com.br",
+    cpfUsuario: "12345678910",
+    senhaUsuario: "556253",
+  }, {
+    nomeCompleto: "Letícia Passos",
+    emailUsuario: "rm555241@fiap.com.br",
+    cpfUsuario: "12345678910",
+    senhaUsuario: "555241",
+  }, {
+    nomeCompleto: "André Altobelli",
+    emailUsuario: "rm554764@fiap.com.br",
+    cpfUsuario: "12345678910",
+    senhaUsuario: "554764",
+  }, ];
+
+let logins = JSON.parse(localStorage.getItem("logins"));
+
+if (logins == null) {
+    localStorage.setItem("logins", JSON.stringify(listaUsuario))
+}
+
+let usuario = JSON.parse(localStorage.getItem("usuario-logado"));
+
+if (usuario) {
+    window.location.href = "../../pages/quiz.html";
+}
